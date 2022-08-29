3ccdb5f94dcf5dc532d5b42af2228ee12cc26e63
DulanjayaSandaruwan <dulanjayasandaruwan1998@gmail.com>
2022-08-29 13:13:20 +0530

    init project

---
+++ src/main/java/com/onlineexammanagementsystembackend/OnlineExamManagementSystemBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.onlineexammanagementsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExamManagementSystemBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineExamManagementSystemBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onlineexammanagementsystembackend/OnlineExamManagementSystemBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlineexammanagementsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExamManagementSystemBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
